a72893a905af888331801353ceed52edd46112b3
Blowa <blowaxd693@gmail.com>
2021-10-26 10:19:21 +0200

    improve drop page (item icons + table)

--- src/components/itemicon.js
+++ src/components/itemicon.js
@@ -1,5 +1,5 @@
 import React from 'react';
 
-export default ({iconId}) => (
-        <img src={'https://nosapki.nostale.club/images/Ikona/' + iconId + '.png'} />
+export default ({iconId, height = null}) => (
+      <img src={'https://nosapki.nostale.club/images/Ikona/' + iconId + '.png'} height={height} />
   );
